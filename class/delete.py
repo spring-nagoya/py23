df8dbf366c17dcf3fc6e6a1cfa9ddc1f8a470f1e
sikigasa <km3t1114@yahoo.co.jp>
2023-10-17 16:42:56 +0900

    fix: bug

--- class/delete.py
+++ class/delete.py
@@ -1,49 +1,49 @@
-
-from flask import Flask,render_template,redirect,request,url_for
+from flask import Flask, render_template, redirect, request, url_for
 
 
 app = Flask(__name__)
 
 import mysql.connector
 
+
 # MySQLへの接続設定を行う関数
 def conn_db():
     # DBへの接続オブジェクトを作成
     conn = mysql.connector.connect(
         # MySQLがどこにあるのか
-        host = "127.0.0.1",
+        host="127.0.0.1",
         # 誰がDBを使うのか
         user="root",
         # DB使用の認証情報
-        passwd = "P@ssw0rd",
+        passwd="P@ssw0rd",
         # 使用するデータベース名
-        db = "py23"
-        
+        db="py23",
     )
     return conn
 
+
 @app.route("/")
 def index():
-    conn=conn_db()
-    cursor=conn.cursor()
+    conn = conn_db()
+    cursor = conn.cursor()
     cursor.execute("SELECT * FROM users")
-    result=cursor.fetchall()
-    return render_template("index.html",record=result)
-    
-    
-@app.route("/delete",methods=["GET"])
+    result = cursor.fetchall()
+    return render_template("index.html", record=result)
+
+
+@app.route("/delete", methods=["GET"])
 def delete():
-    conn=""
-    cursor=""
+    conn = ""
+    cursor = ""
     try:
-        id=request.args.get("id")
-        conn=conn_db()
-        cursor=conn.cursor()
-        cursor.execute("DELETE FROM T_sample WHERE id=0".format(id))
+        id = request.args.get("id")
+        conn = conn_db()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM T_sample WHERE id= {0}".format(id))
         conn.commit()
     except mysql.connector.Error as e:
         print(e)
-        
+
     finally:
         if cursor:
             cursor.close()
